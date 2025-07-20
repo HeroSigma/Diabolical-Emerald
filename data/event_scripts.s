9b6bd6640381af9f5caccadcea021bc99216460f
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-07-19 20:57:25 -0500

    Include Kanto map scripts

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -582,6 +582,7 @@ gStdScripts_End::
 	.include "data/maps/Route119_WeatherInstitute_2F/scripts.inc"
 	.include "data/maps/Route119_House/scripts.inc"
 	.include "data/maps/Route124_DivingTreasureHuntersHouse/scripts.inc"
+    .include "data/event_scripts_cross.inc"
 
 	.include "data/scripts/std_msgbox.inc"
 	.include "data/scripts/trainer_battle.inc"
