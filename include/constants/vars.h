232d965a97ed265cd7303908fbf2791328e4ab13
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-07-20 19:16:56 -0500

    fix: silence macro redefinition warnings

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -293,8 +293,11 @@
 #define VARS_COUNT                                       (VARS_END - VARS_START + 1)
 
 // Johto variable aliases using unused Hoenn vars
+#undef VAR_NEW_BARK_TOWN_STATE
 #define VAR_NEW_BARK_TOWN_STATE  VAR_DEWFORD_TOWN_STATE
+#undef VAR_ROUTE29_STATE
 #define VAR_ROUTE29_STATE        VAR_PACIFIDLOG_TOWN_STATE
+#undef VAR_ELM_LAB_STATE
 #define VAR_ELM_LAB_STATE        VAR_VERDANTURF_TOWN_STATE
 
 #define SPECIAL_VARS_START            0x8000
