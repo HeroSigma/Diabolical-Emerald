ffd037d97d4bbe4a9cc702ad15558c397ee7d51c
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-07-30 15:29:50 -0500

    Update field_camera.c

--- src/field_camera.c
+++ src/field_camera.c
@@ -242,7 +242,7 @@ static void DrawMetatileAt(const struct MapLayout *mapLayout, u16 offset, int x,
     DrawMetatile(MapGridGetMetatileLayerTypeAt(x, y), metatiles + metatileId * NUM_TILES_PER_METATILE, offset);
 }
 
-
+static void DrawMetatile(s32, const u16 *, u16)
 {
     if (metatileLayerType == 0xFF)
     {
